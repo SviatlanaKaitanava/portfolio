a8ae7e2e3b73bbfedf3e662e164035c067324f02
SviatlanaKaitanava <42001084+SviatlanaKaitanava@users.noreply.github.com>
2020-08-21 14:02:29 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+new WOW().init();
+var mySwiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.project-bullets',
+        bulletClass: 'project-bullet',
+        bulletActiveClass: 'project-bullet-active'
+    },
+});
